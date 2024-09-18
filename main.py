f3b20eb5ae55cc0774c76b30d958ed4fa6aa968b
Konstantin Greif <kgreif@dataguard.de>
2024-09-18 18:27:00 +0200

    Add player and rotation

--- main.py
+++ main.py
@@ -2,20 +2,33 @@ import pygame
 from pygame.locals import *
 
 from constants import *
+from player import Player
 
 def main():
+    print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+
+    player_x = SCREEN_WIDTH / 2
+    player_y = SCREEN_HEIGHT / 2
+    player = Player(player_x, player_y)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, color=(0,0,0))
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+
+        delta = clock.tick(60)
+        dt = delta / 1000
+
 
 if __name__ == "__main__":
     main()

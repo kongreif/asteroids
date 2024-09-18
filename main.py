6a03780438d625155157359980f6771201b40b2e
Konstantin Greif <kgreif@dataguard.de>
2024-09-18 18:52:23 +0200

    Add spawning asteroids

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -12,7 +14,18 @@ def main():
 
     player_x = SCREEN_WIDTH / 2
     player_y = SCREEN_HEIGHT / 2
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
     player = Player(player_x, player_y)
+
+    Asteroid.containers = (asteroids, updateable, drawable)
+
+    AsteroidField.containers = (updateable)
+    asteroid_field = AsteroidField()
 
     dt = 0
 
@@ -22,8 +35,11 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for item in updateable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
         delta = clock.tick(60)
